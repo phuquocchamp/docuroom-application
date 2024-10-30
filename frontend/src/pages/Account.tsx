39e05215de2daed9e9e98b13bc724ecb1de3992e
phuquocchamp <hoangtanphuquoc@gmail.com>
2024-10-31 00:40:54 +0700

    upadate all pages

---
+++ frontend/src/pages/Account.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "../components/Sidebar"
+
+function Account() {
+  return (
+    <div>
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default Account
---
+++ frontend/src/pages/StudyGroup.tsx
@@ -0,0 +1,11 @@
+import Sidebar from '../components/Sidebar'
+
+function StudyGroup() {
+  return (
+    <div>
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default StudyGroup
