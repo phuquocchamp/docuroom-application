3e879d33911e48f5e53141d1dd182cf04e7b3570
phuquocchamp <hoangtanphuquoc@gmail.com>
2024-10-31 11:06:40 +0700

    update code base

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,26 +1,41 @@
 import { Link } from "react-router-dom";
+import { IoSettings } from "react-icons/io5";
+import { MdPersonPin, MdSpaceDashboard } from "react-icons/md";
+import { HiDocumentText } from "react-icons/hi2";
+import { BiSolidGroup } from "react-icons/bi";
+import { TbHelpHexagonFilled } from "react-icons/tb";
 
 function Sidebar() {
   return (
-    <div className="w-64 h-screen bg-gray-800 text-white flex flex-col justify-between">
+    <div className="w-64 h-screen bg-white text-light-purpe flex flex-col justify-between">
       <nav className="mt-8">
-        <ul className="space-y-4 px-4">
-          <li className="hover:bg-gray-700 p-2 rounded">
+        <div className="bg-white rounded border font-medium text-xl text-black p-4 my-4 rounded text-center">
+          <div>Dashboard</div>
+        </div>
+        <ul className="space-y-4 px-4 text-lg">
+
+          <li className="hover:bg-fresh-blue hover:text-white rounded p-3 flex items-center">
+            <MdSpaceDashboard className="m-2"/>
             <Link to="/">Dashboard</Link>
           </li>
-          <li className="hover:bg-gray-700 p-2 rounded">
+          <li className="hover:bg-fresh-blue hover:text-white rounded p-3 flex items-center">
+            <HiDocumentText className="m-2"/>
             <Link to="/document">Document</Link>
           </li>
-          <li className="hover:bg-gray-700 p-2 rounded">
+          <li className="hover:bg-fresh-blue hover:text-white rounded p-3 flex items-center">
+            <BiSolidGroup className="m-2"/>
             <Link to="/study-group">Study Group</Link>
           </li>
-          <li className="hover:bg-gray-700 p-2 rounded">
+          <li className="hover:bg-fresh-blue hover:text-white rounded p-3 flex items-center">
+            <MdPersonPin className="m-2"/>
             <Link to="/account">My Account</Link>
           </li>
-          <li className="hover:bg-gray-700 p-2 rounded">
+          <li className="hover:bg-fresh-blue hover:text-white rounded p-3 flex items-center">
+            <IoSettings className="m-2"/>
             <Link to="/setting">Settings</Link>
           </li>
-          <li className="hover:bg-gray-700 p-2 rounded">
+          <li className="hover:bg-fresh-blue hover:text-white rounded p-3 flex items-center">
+            <TbHelpHexagonFilled className="m-2"/>
             <Link to="/help">Help</Link>
           </li>
         </ul>
