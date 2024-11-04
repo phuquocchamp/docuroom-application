2066444af80fe8c5aca43f6118e916550871e101
klitn <klinhtruong04@gmail.com>
2024-11-04 10:59:01 +0700

    update routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,19 +5,30 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import StudyGroup from './pages/StudyGroup';
 import Setting from './pages/Setting';
 import Help from './pages/Help';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ForgotPassword from './pages/ForgotPassword';
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<Layout />}>
-    <Route index element={<Home/>} />
-    <Route path='document' element={<Document/>} />
-    <Route path='studygroup' element={<StudyGroup/>} />
-    <Route path='myaccount' element={<Document/>} />
-    <Route path='setting' element={<Setting/>} />
-    <Route path='help' element={<Help/>} />
-    </Route>
-    </Routes>
+      <Routes>
+
+        <Route path='/'>
+          <Route path='' element={<Login />} />
+          <Route path='signup' element={<Register />} />
+          <Route path='forgotpassword' element={<ForgotPassword />} />
+        </Route>
+
+        <Route path="/home" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='document' element={<Document />} />
+          <Route path='studygroup' element={<StudyGroup />} />
+          <Route path='myaccount' element={<Document />} />
+          <Route path='setting' element={<Setting />} />
+          <Route path='help' element={<Help />} />
+        </Route>
+
+      </Routes>
     </BrowserRouter>
   );
 }
